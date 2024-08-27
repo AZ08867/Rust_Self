bdbce06bb41ce5b91948f3c947a0e0906b93a753
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-27 21:11:13 +0800

    add events

---
+++ events_rust/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    // println!("halo world");
+    // println!("你好 Rust");
+    // println!("this is a test!!")
+
+    // let 变量名 = 值
+    // let food = "清蒸鲈鱼";
+    // let price = 300;
+    // let checked = true;
+    //
+    // println!("food is {}", food);
+    // println!("price is {}", price);
+    // if checked == true {
+    //     println!("check is done!")
+    // }
+
+
+    // 整型
+    // let price = 100;
+    // let price2 = 200;
+    // let price3 = -300;
+    // let price4 = 400;
+    // let price5 = 500;
+    //
+    // println!("price is {}", price);
+    // println!("price is {} and price3 is {}", price2, price3);
+    // println!("price4 is {} and price5 is {}", price4, price5);
+
+
+    // 浮点型
+    let price9 = 18.00;
+    let price10 = 8.88;
+    let price11 = 168.125;
+    println!("price9 is {} and price10 is {} and price11 is {}", price9, price10, price11);
+
+    let price12 = 1_000_000;
+    let price13 = 1_000_000.666_123;
+    println!("price12 is {0} and price13 is {1}", price12, price13);
+}
