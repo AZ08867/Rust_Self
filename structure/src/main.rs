6642cf89c4d24f8725efbd8901835adc8e69441e
AZKvgkv <153256707+AZKvgkv@users.noreply.github.com>
2024-08-27 21:12:01 +0800

    add structure

---
+++ structure/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+    /*
+    三种结构体
+    1.元组结构体
+    2.单元结构体
+    3.C++风格的结构体
+    */
+}
+// //  1.元组结构体
+// struct Color(i32, i32, i32);
+
+// // 2. 单元结构体
+// struct Empty;
+
+// // 3. C++风格的结构体
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
